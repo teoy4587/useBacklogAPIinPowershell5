a193946274fcc3df61fe9baa8cf88bdcb85e0f43
teoy4587 <teoy4587@gmail.com>
2025-04-02 20:48:51 +0900

    patchを使用するAPIが実行できることを確認したためコミット

--- src/BacklogApiExetcuteHelper.ps1
+++ src/BacklogApiExetcuteHelper.ps1
@@ -1,9 +1,10 @@
 # 引数定義　
 Param(
-  [parameter(Mandatory = $true)][ValidateSet("get", "post")][string]$Arg1, #HTTPmethod(GETまたはPOST)
-  [parameter(Mandatory = $true)][ValidateSet("issues", "projects", "wikis")][string]$Arg2, #issuesなどのURL部分
-  [string]$Arg3, #Body経由で引き渡すパラメーター
-  [string]$Arg4 #URLに組み込むパラメーター
+  [parameter(Mandatory = $true)][ValidateSet("get", "post", "patch")][string]$Arg1, #HTTPmethod(GETまたはPOST)
+  [parameter(Mandatory = $true)][ValidateSet("issues", "projects", "wikis", "watchings")][string]$Arg2, #issuesなどのURL部分
+  [string]$Arg3, #URLに組み込むパラメーター
+  [string]$Arg4, #body経由で引き渡すパラメーター名
+  [string]$Arg5 #body経由で引き渡すパラメーター
 )
 
 # 設定ファイルの読み込み
@@ -15,10 +16,11 @@ function BacklogApiExetcuteHelper {
   try {
     # 入力値を元にAPIを実行する
     $url = makeUrl $Arg2 $Arg3
+    Write-Host $url
     if($arg1 -eq "get"){
     $res = executeApIGet $url
   } else {
-    $res = executeApIExceptGet $url $Arg1
+    $res = executeApIExceptGet $url $Arg1 $Arg4 $Arg5
   }
     makeResultFile $res
   }
@@ -43,7 +45,7 @@ function makeUrl {
   }
   else {
     #
-    $urlStrings = $urlBase + $target + $apiKey
+    $urlStrings = $urlBase + $target + '/' + $params + $apiKey
   }
 
   return $urlStrings
@@ -59,8 +61,10 @@ function executeApIGet {
 function executeApIExceptGet {
   Param ([parameter(Mandatory = $true)]$urlFull, [parameter(Mandatory = $true)][string]$method, [string]$parameterName, [string]$parameter)
 
+  if ($null -ne $parameterName){
     $headers = @{"Content-Type" = "application/x-www-form-urlencoded"}
     $params = @{"$parameterName" = "$parameter"}
+  }
 
     return Invoke-RestMethod -uri $urlFull  -method $method -Headers $headers -Body $params -ErrorAction Stop
 
